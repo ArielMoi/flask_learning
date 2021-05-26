03828e6d78b91d7d3a71b98ea0eda2774ac98cee
Ariel Moise <ariel.m23@gmail.com>
2021-05-26 11:26:15 +0300

    continue

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'splite:///users.splite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
@@ -10,20 +17,48 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
         session['user'] = user
+        flash(f"You have logged in, {user}", "info")
         return redirect(url_for('user'))
     else:
+        if 'user' in session:
+            flash(f"Already logged in", "info")
+            return redirect(url_for('user'))
         return render_template('login.html')
 
 
-@app.route('/user')
+@app.route('/user', methods=['GET', 'POST'])
 def user():
+    email = None
     if 'user' in session:
         user = session['user']
-        return f'<h1>{user}</h1>'
+        
+        if request.method == 'POST':
+            email = request.form['email']
+            session['email'] = email
+            flash('email was saved')
+        else:
+            if 'email' in session:
+                email = session['email']
+            
+        return render_template('user.html', email=email)
     else:
+        flash(f"you are not logged in", "info")
         return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    if 'user' in session:
+        user = session['user']
+        flash(f"You have been logged out, {user}", "info")
+        
+    session.pop('user', None)
+    session.pop('email', None)
+    
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
